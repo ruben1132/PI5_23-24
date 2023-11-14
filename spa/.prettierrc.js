e14dbc76090caed7ebc40ba0a3f92aba4386c20a
1191362 <1191362@isep.ipp.pt>
2023-11-14 22:05:14 +0000

    updates

--- spa/.prettierrc.js
+++ spa/.prettierrc.js
@@ -6,14 +6,5 @@ module.exports = {
     tabWidth: 4,
     useTabs: false, // Set this to true if you want to use tabs for indentation
 
-    overrides: [
-      {
-        files: '*.json',
-        options: {
-          // JSON-specific configurations here
-          bracketSpacing: true, // Add space inside brackets [1, 2, 3]
-          arrayBracketSpacing: true, // Add space inside brackets [1, 2, 3]
-        },
-      },
-    ],
+
   };
