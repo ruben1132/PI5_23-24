60b7dfe8c790faec9c07f7d9fe1faf56e12bc251
André L Reis <gato5andre26@gmail.com>
2023-11-15 15:09:30 +0000

    mgi api: changed floor map dto

--- mgi/src/dto/IFloorMapDTO.ts
+++ mgi/src/dto/IFloorMapDTO.ts
@@ -1,34 +1,13 @@
-interface Size {
-    width: number;
-    height?: number;
-    depth: number;
-}
-
-interface Position {
-    positionX: number;
-    positionY: number;
-    direction?: string;
-}
-
 export interface IFloorMapDTO {
     floor: string;
-    fmDoors: {
-        position: Position;
-    }[];
     fmElevator: {
         elevatorId: string;
-        position: Position;
     };
     fmRooms: {
         roomId: string;
-        startX: number;
-        startY: number;
-        endX: number;
-        endY: number;
     }[];
     fmPassages: {
         passageId: string;
-        position: Position;
     }[];
 }
 
