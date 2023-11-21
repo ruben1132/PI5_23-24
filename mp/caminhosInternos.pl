423b41188d5721e146473baf40c2bce278d0e296
André L Reis <gato5andre26@gmail.com>
2023-11-21 12:40:09 +0000

    #47 fix: changed the diagonals checks

--- mp/caminhosInternos.pl
+++ mp/caminhosInternos.pl
@@ -1,80 +1,12 @@
 % ALGORITMOS PARA OS MOVIMENTOS DO ROBO NO INTERIOR DE CADA PISO
 :-dynamic ligacel/2.
 
-%linha 1:1,1,1,1,1,1,1,1
-%linha 2:0,0,0,0,0,0,0,1
-%linha 3:0,0,0,0,0,0,0,1
-%linha 4:0,0,0,0,0,0,0,1
-%linha 5:1,1,1,1,0,0,0,1
-%linha 6:1,1,1,1,0,0,0,1
-%linha 7:1,1,1,1,0,0,0,1
-%coluna :1,2,3,4,5,6,7,8
-%
-%
-%
-%m(col,lin,valor)
-m(1,1,1).
-m(2,1,1).
-m(3,1,1).
-m(4,1,1).
-m(5,1,1).
-m(6,1,1).
-m(7,1,1).
-m(8,1,1).
+% floorMapsBC.pl - mapas dos pisos
+:- consult('floorMapsBC.pl').
 
-m(1,2,0).
-m(2,2,0).
-m(3,2,0).
-m(4,2,0).
-m(5,2,0).
-m(6,2,0).
-m(7,2,0).
-m(8,2,1).
+% coordenadasBC.pl - coordenadas dos elevadores, salas, passagens
+:- consult('coordenadasBC.pl').
 
-m(1,3,0).
-m(2,3,0).
-m(3,3,0).
-m(4,3,0).
-m(5,3,0).
-m(6,3,0).
-m(7,3,0).
-m(8,3,1).
-
-m(1,4,0).
-m(2,4,0).
-m(3,4,0).
-m(4,4,0).
-m(5,4,0).
-m(6,4,0).
-m(7,4,0).
-m(8,4,1).
-
-m(1,5,1).
-m(2,5,1).
-m(3,5,1).
-m(4,5,1).
-m(5,5,0).
-m(6,5,0).
-m(7,5,0).
-m(8,5,1).
-
-m(1,6,1).
-m(2,6,1).
-m(3,6,1).
-m(4,6,1).
-m(5,6,0).
-m(6,6,0).
-m(7,6,0).
-m(8,6,1).
-
-m(1,7,1).
-m(2,7,1).
-m(3,7,1).
-m(4,7,1).
-m(5,7,0).
-m(6,7,0).
-m(7,7,0).
-m(8,7,1).
 
 cria_grafo(_,0):-!.
 cria_grafo(Col,Lin):-cria_grafo_lin(Col,Lin),Lin1 is Lin-1,cria_grafo(Col,Lin1).
@@ -86,10 +18,10 @@ cria_grafo_lin(Col,Lin):-m(Col,Lin,0),!,ColS is Col+1, ColA is Col-1, LinS is Li
     ((m(ColA,Lin,0),assertz(ligacel(cel(Col,Lin),cel(ColA,Lin)));true)),
     ((m(Col,LinS,0),assertz(ligacel(cel(Col,Lin),cel(Col,LinS)));true)),
     ((m(Col,LinA,0),assertz(ligacel(cel(Col,Lin),cel(Col,LinA)));true)),
-    ((m(ColS, LinS, 0), assertz(ligacel(cel(Col, Lin), cel(ColS, LinS)))); true),
-    ((m(ColA, LinA, 0), assertz(ligacel(cel(Col, Lin), cel(ColA, LinA)))); true),
-    ((m(ColA, LinS, 0), assertz(ligacel(cel(Col, Lin), cel(ColA, LinS)))); true),
-    ((m(ColS, LinA, 0), assertz(ligacel(cel(Col, Lin), cel(ColS, LinA)))); true).
+	((m(ColS,LinS,0), m(ColS,Lin,0), m(Col,LinS,0), assertz(ligacel(cel(Col,Lin),cel(ColS,LinS)));true)),
+	((m(ColA,LinA,0), m(ColA,Lin,0), m(Col,LinA,0), assertz(ligacel(cel(Col,Lin),cel(ColA,LinA)));true)),
+	((m(ColA,LinS,0), m(ColA,Lin,0), m(Col,LinS,0), assertz(ligacel(cel(Col,Lin),cel(ColA,LinS)));true)),
+	((m(ColS,LinA,0), m(ColS,Lin,0), m(Col,LinA,0), assertz(ligacel(cel(Col,Lin),cel(ColS,LinA)));true)),
     Col1 is Col-1,
     cria_grafo_lin(Col1,Lin).
 cria_grafo_lin(Col,Lin):-Col1 is Col-1,cria_grafo_lin(Col1,Lin).
