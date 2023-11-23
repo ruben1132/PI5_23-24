be2500db311b80b678a403bcb524695d4b69f89f
André L Reis <gato5andre26@gmail.com>
2023-11-23 20:59:04 +0000

    #69 wip: integration predicate fcks all the ligacel but if i exec the predicates one by one, it works

--- mp/campusBC.pl
+++ mp/campusBC.pl
@@ -8,18 +8,18 @@ pisos(b,[b1,b2,b3]).
 pisos(c,[c1,c2,c3,c4]).
 pisos(d,[d1,d2,d3]).
 
-dimensoes(a1, 22, 10).
-dimensoes(a2, 22, 10).
-dimensoes(b1, 22, 10).
-dimensoes(b2, 22, 10).
-dimensoes(b3, 22, 10).
-dimensoes(c1, 12, 20).
-dimensoes(c2, 12, 20).
-dimensoes(c3, 12, 20).
-dimensoes(c4, 12, 20).
-dimensoes(d1, 12, 20).
-dimensoes(d2, 12, 20).
-dimensoes(d3, 12, 20).
+dimensoes(a1, 23, 11).
+dimensoes(a2, 23, 11).
+dimensoes(b1, 23, 11).
+dimensoes(b2, 23, 11).
+dimensoes(b3, 23, 11).
+dimensoes(c1, 13, 21).
+dimensoes(c2, 13, 21).
+dimensoes(c3, 13, 21).
+dimensoes(c4, 13, 21).
+dimensoes(d1, 13, 21).
+dimensoes(d2, 13, 21).
+dimensoes(d3, 13, 21).
 
 elevador(a,[a1,a2]).
 elevador(b,[b1,b2,b3]).
@@ -42,32 +42,32 @@ sala(r1, a1).
 sala(r2, a1).
 
 % Salas Edificio A - Piso 2
-sala(a201, a2).
-sala(a202, a2).
-sala(a203, a2).
-sala(a204, a2).
-sala(a205, a2).
-sala(a206, a2).
-sala(a207, a2).
-sala(a207b, a2).
-sala(a209, a2).
+sala(a211, a2).
+sala(a212, a2).
+sala(a213, a2).
+sala(a214, a2).
+sala(a215, a2).
+sala(a216, a2).
+sala(a217, a2).
+sala(a217b, a2).
+sala(a219, a2).
 
 % Salas Edificio B - Piso 1
-sala(b101, b1).
-sala(b102, b1).
-sala(b103, b1).
-sala(b104, b1).
-sala(b105, b1).
-sala(b106, b1).
-sala(b107, b1).
-sala(b108, b1).
+sala(b111, b1).
+sala(b113, b1).
+sala(b113, b1).
+sala(b114, b1).
+sala(b115, b1).
+sala(b116, b1).
+sala(b117, b1).
+sala(b118, b1).
 
 % Salas Edificio B - Piso 2
-sala(b201, b2).
-sala(b202, b2).
-sala(b203, b2).
-sala(b205, b2).
-sala(b207, b2).
+sala(b211, b2).
+sala(b212, b2).
+sala(b213, b2).
+sala(b215, b2).
+sala(b217, b2).
 
 % Salas Edificio B - Piso 3
 sala(b301, b3).
@@ -76,24 +76,24 @@ sala(b303, b3).
 sala(b305, b3).
 
 % Salas Edificio C - Piso 1
-sala(c101, c1).
-sala(c103, c1).
-sala(c105, c1).
-sala(c102, c1).
-sala(c104, c1).
-sala(c106, c1).
-sala(c108, c1).
-sala(c110, c1).
+sala(c111, c1).
+sala(c113, c1).
+sala(c115, c1).
+sala(c113, c1).
+sala(c114, c1).
+sala(c116, c1).
+sala(c118, c1).
+sala(c111, c1).
 
 % Salas Edificio C - Piso 2
-sala(c201, c2).
-sala(c202, c2).
-sala(c203, c2).
-sala(c204, c2).
-sala(c205, c2).
-sala(c206, c2).
-sala(c207, c2).
-sala(c209, c2).
+sala(c211, c2).
+sala(c212, c2).
+sala(c213, c2).
+sala(c214, c2).
+sala(c215, c2).
+sala(c216, c2).
+sala(c217, c2).
+sala(c219, c2).
 
 % Salas Edificio C - Piso 3
 sala(c301, c3).
@@ -103,7 +103,7 @@ sala(c304, c3).
 sala(c305, c3).
 sala(c306, c3).
 sala(c308, c3).
-sala(c310, c3).
+sala(c311, c3).
 
 % Salas Edificio C - Piso 4
 sala(c401, c4).
@@ -112,21 +112,21 @@ sala(c403, c4).
 sala(c404, c4).
 
 % Salas Edificio D - Piso 1
-sala(d101, d1).
-sala(d103, d1).
-sala(d105, d1).
-sala(d102, d1).
-sala(d104, d1).
-sala(d106, d1).
-sala(d108, d1).
+sala(d111, d1).
+sala(d113, d1).
+sala(d115, d1).
+sala(d113, d1).
+sala(d114, d1).
+sala(d116, d1).
+sala(d118, d1).
 
 % Salas Edificio D - Piso 2
-sala(d201, d2).
-sala(d202, d2).
-sala(d203, d2).
-sala(d204, d2).
-sala(d205, d2).
-sala(d206, d2).
+sala(d211, d2).
+sala(d212, d2).
+sala(d213, d2).
+sala(d214, d2).
+sala(d215, d2).
+sala(d216, d2).
 
 % Salas Edificio D - Piso 3
 sala(d301, d3).
