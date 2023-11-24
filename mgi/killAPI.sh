2dcfb7ccf484e361578ca99c9073dca2aeb94c5b
1191362 <1191362@isep.ipp.pt>
2023-11-24 01:22:59 +0000

    Rel lapr5 added
    Rel lapr5 added

--- mgi/killAPI.sh
+++ mgi/killAPI.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# # Hardcoded port number
-# PORT_NUMBER=4000
+# Hardcoded port number
+PORT_NUMBER=4000
 
-# # Get the list of processes using the specified port
-# PIDS=$(lsof -t -i :$PORT_NUMBER)
+# Get the list of processes using the specified port
+PIDS=$(lsof -t -i :$PORT_NUMBER)
 
-# # Check if any processes are using the specified port
-# if [ -z "$PIDS" ]; then
-#   echo "No processes found using port $PORT_NUMBER"
-# else
-#   # Kill the processes
-#   echo "Killing processes using port $PORT_NUMBER: $PIDS"
-#   kill -9 -f $PIDS
-# fi
+# Check if any processes are using the specified port
+if [ -z "$PIDS" ]; then
+  echo "No processes found using port $PORT_NUMBER"
+else
+  # Kill the processes
+  echo "Killing processes using port $PORT_NUMBER: $PIDS"
+  kill -9 -f $PIDS
+fi
 
-# exit 0
+exit 0
 
 pkill -9 -f "node"
--- mgi/runAPI.sh
+++ mgi/runAPI.sh
@@ -1,8 +1,23 @@
 #! /bin/sh
 
+sh killAPI.sh
 
-cd /home/ubuntu/API/repo/mgi
+cd /home/ubuntu/API/
 
-sudo npm i
+rm -rf repo
+
+git clone --depth 1 --filter=blob:none --no-checkout https://ruben1132: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@github.com/ruben1132/PI5_23-24.git repo
+
+cd repo
+
+git sparse-checkout init --cone
+
+git sparse-checkout set mgi
+
+git pull
+
+cd mgi
+
+npm install
 
 sudo nohup npm run start &
