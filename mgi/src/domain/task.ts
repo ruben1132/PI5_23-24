9c96c087738e5a1ebe2de49f9b9a8d178cdf396f
MileneFarias <130046426+MileneFarias@users.noreply.github.com>
2023-11-25 12:27:41 +0000

    #63 alteraçao no modulo mgi e no form das tasks

--- mgi/src/domain/task.ts
+++ mgi/src/domain/task.ts
@@ -1,61 +1,58 @@
 import { AggregateRoot } from "../core/domain/AggregateRoot";
 import { UniqueEntityID } from "../core/domain/UniqueEntityID";
-
 import { Result } from "../core/logic/Result";
 import { TaskId } from "./valueObj/taskId";
 import { Guard } from "../core/logic/Guard";
-
-
+import { TaskTypeId } from "./valueObj/taskTypeId";
+import { RobotId } from "./valueObj/robotId";
 
 interface TaskProps {
-    id: string;
-    initialType: string;
-    finalType : string;
-    path : string;
+    designation: string; // TODO: criar um value obj para designacoes (meter um max de chars por exemplo)
+    type: TaskTypeId;
+    assigned: RobotId;
+   
 }
-
 export class Task extends AggregateRoot<TaskProps> {
     get id(): UniqueEntityID {
         return this._id;
     }
-
-    get domainId(): TaskId {
+    get taskId(): TaskId {
         return new TaskId(this.id.toValue());
     }
-
-    get initialType(): string {
-        return this.props.initialType;
+    get designation(): string {
+        return this.props.designation;
+   
     }
-
-    get finalType(): string {
-        return this.props.finalType;
+    get assigned(): RobotId {
+        return this.props.assigned;
+   
     }
-
-    get path(): string {
-        return this.props.path;
+    get type(): TaskTypeId {
+        return this.props.type;
+   
     }
-
     private constructor(props: TaskProps, id?: UniqueEntityID) {
         super(props, id);
     }
-
     public static create(props: TaskProps, id?: UniqueEntityID): Result<Task> {
-
         const guardedProps = [
-            { argument: props.initialType, argumentName: 'initialType' },
-            { argument: props.finalType, argumentName: 'finalType' },
+            { argument: props.designation, argumentName: 'designation' },
+            { argument: props.type, argumentName: 'type' },
+            { argument: props.assigned, argumentName: 'assigned' }
+            
            
         ];
         const guardResult = Guard.againstNullOrUndefinedBulk(guardedProps);
-
         if (!guardResult.succeeded) {
             return Result.fail<Task>(guardResult.message)
         }
-
-        const task = new Task({id : props.id,initialType: props.initialType,
-            finalType: props.finalType,path : props.path});
-
-        return Result.ok<Task>(task)
-
+        const role = new Task({
+            designation: props.designation,
+            type: props.type,
+            assigned: props.assigned
+        }, id);
+       
+        return Result.ok<Task>(role)
+        
     }
 }
