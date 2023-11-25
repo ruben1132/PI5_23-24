496a33989992723981d578dfb6f232eeda9d3ff7
1191362 <1191362@isep.ipp.pt>
2023-11-25 16:49:43 +0000

    Update next.config.js

--- spa/next.config.js
+++ spa/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    // Other Next.js configuration...
+  
+    server: {
+      // Override the default Next.js server
+      // Use HTTP server instead of HTTPS
+      http: {
+        // Force HTTP in production
+        force: true,
+      },
+    },
+  };
 
 module.exports = nextConfig;
