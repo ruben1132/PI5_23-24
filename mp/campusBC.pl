819e8a6c9c07f0df17ca82f20ef19946cab8b9fa
1191362 <1191362@isep.ipp.pt>
2023-11-26 14:21:54 +0000

    Update campusBC.pl

--- mp/campusBC.pl
+++ mp/campusBC.pl
@@ -42,32 +42,32 @@ sala(r1, a1).
 sala(r2, a1).
 
 % Salas Edificio A - Piso 2
-sala(a211, a2).
-sala(a212, a2).
-sala(a213, a2).
-sala(a214, a2).
-sala(a215, a2).
-sala(a216, a2).
-sala(a217, a2).
-sala(a217b, a2).
-sala(a219, a2).
+sala(a201, a2).
+sala(a202, a2).
+sala(a203, a2).
+sala(a204, a2).
+sala(a205, a2).
+sala(a206, a2).
+sala(a207, a2).
+sala(a207b, a2).
+sala(a209, a2).
 
 % Salas Edificio B - Piso 1
-sala(b111, b1).
-sala(b113, b1).
-sala(b113, b1).
-sala(b114, b1).
-sala(b115, b1).
-sala(b116, b1).
-sala(b117, b1).
-sala(b118, b1).
+sala(b101, b1).
+sala(b102, b1).
+sala(b103, b1).
+sala(b104, b1).
+sala(b105, b1).
+sala(b106, b1).
+sala(b107, b1).
+sala(b108, b1).
 
 % Salas Edificio B - Piso 2
-sala(b211, b2).
-sala(b212, b2).
-sala(b213, b2).
-sala(b215, b2).
-sala(b217, b2).
+sala(b201, b2).
+sala(b202, b2).
+sala(b203, b2).
+sala(b205, b2).
+sala(b207, b2).
 
 % Salas Edificio B - Piso 3
 sala(b301, b3).
@@ -76,24 +76,24 @@ sala(b303, b3).
 sala(b305, b3).
 
 % Salas Edificio C - Piso 1
-sala(c111, c1).
-sala(c113, c1).
-sala(c115, c1).
-sala(c113, c1).
-sala(c114, c1).
-sala(c116, c1).
-sala(c118, c1).
-sala(c111, c1).
+sala(c101, c1).
+sala(c102, c1).
+sala(c103, c1).
+sala(c104, c1).
+sala(c105, c1).
+sala(c106, c1).
+sala(c108, c1).
+sala(c110, c1).
 
 % Salas Edificio C - Piso 2
-sala(c211, c2).
-sala(c212, c2).
-sala(c213, c2).
-sala(c214, c2).
-sala(c215, c2).
-sala(c216, c2).
-sala(c217, c2).
-sala(c219, c2).
+sala(c201, c2).
+sala(c202, c2).
+sala(c203, c2).
+sala(c204, c2).
+sala(c205, c2).
+sala(c206, c2).
+sala(c207, c2).
+sala(c209, c2).
 
 % Salas Edificio C - Piso 3
 sala(c301, c3).
@@ -103,7 +103,7 @@ sala(c304, c3).
 sala(c305, c3).
 sala(c306, c3).
 sala(c308, c3).
-sala(c311, c3).
+sala(c310, c3).
 
 % Salas Edificio C - Piso 4
 sala(c401, c4).
@@ -113,10 +113,10 @@ sala(c404, c4).
 
 % sala Salas Edificio D - Piso 1
 sala(d101, d1).
-sala(d103, d1).
-sala(d105, d1).
 sala(d102, d1).
+sala(d103, d1).
 sala(d104, d1).
+sala(d105, d1).
 sala(d106, d1).
 sala(d108, d1).
 
