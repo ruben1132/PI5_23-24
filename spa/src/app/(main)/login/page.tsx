5044bdf1ac7a6ae5978e7c9efdafcc3adf0daac0
1191362 <64978968+ruben1132@users.noreply.github.com>
2023-11-27 11:32:14 +0000

    Small update

--- spa/src/app/(main)/login/page.tsx
+++ spa/src/app/(main)/login/page.tsx
@@ -4,20 +4,20 @@ import LoginForm from '@/components/forms/LoginForm';
 
 export default function Login() {
     return (
-            <Container className="d-flex flex-column align-items-center justify-content-center min-vh-100">
-                {/* Logo */}
-                <Row className="mb-4">
+        <Container className="d-flex flex-column align-items-center justify-content-center min-vh-100">
+            {/* Logo */}
+            {/* <Row className="mb-4">
                     <Col>
                         <Image src="/images/logo.png" alt="Logo" width={90} height={100} />
                     </Col>
-                </Row>
+                </Row> */}
 
-                {/* User Form */}
-                <Row>
-                    <Col>
-                        <LoginForm />
-                    </Col>
-                </Row>
-            </Container>
+            {/* User Form */}
+            <Row>
+                <Col>
+                    <LoginForm />
+                </Col>
+            </Row>
+        </Container>
     );
 }
