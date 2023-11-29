746eb48db5056292317eccf1ab48cf3ebdaa1d64
André L Reis <gato5andre26@gmail.com>
2023-11-29 09:50:06 +0000

    updated passages - not ideal

--- mp/coordenadasBC.pl
+++ mp/coordenadasBC.pl
@@ -118,7 +118,7 @@ coordenadas(d3, 1, 1).
 % Coordenadas das passagens
 coordenadas(c2,d2, 0, 14, 12, 0).
 coordenadas(a2,b2, 22, 5, 0, 4).
-coordenadas(b2,c3, 22, 8, 0, 8).
-coordenadas(b2,d3, 21, 10, 10, 0).
+coordenadas(b2,c3, 2, 8, 0, 8).
+coordenadas(b2,d3, 2, 9, 10, 0).
 coordenadas(d3,c3, 12, 1, 0, 14).
 coordenadas(b3,c4, 22, 8, 0, 8).
